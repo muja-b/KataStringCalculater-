3cd7cca614cd249ebed591556e70f5d9b13fd54f
muja-b <s11819212@stu.najah.edu>
2022-08-08 23:37:52 +0300

    Init commit

---
+++ ICalculater.cs
@@ -0,0 +1,4 @@
+interface ICalculater
+{
+  int add(string Numbers); // interface method (does not have a body)
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Kata
+{
+    class StringCalc
+    {
+        static void Main(string[] args)
+        {
+            KataStringCalcolator FactoredCalc = new KataStringCalcolator();
+            int ans = FactoredCalc.add("1,2,3");
+            Console.WriteLine(ans);
+        }
+
+    }
+    class KataStringCalcolator : ICalculater
+    {
+        public int add(string Numbers)
+        {
+
+            try
+            {
+                 return tryAdd(Numbers);
+            }
+            catch (System.Exception)
+            {
+                return 0; 
+            }
+
+        } 
+        private int tryAdd(string Numbers)
+        {
+            int ans;
+            ans = Numbers.Where(x=>x!=',').Aggregate((sum, i) => sum += i);
+            ans-=Numbers.Where(x=>x!=',').Count()*'0';
+            return ans;
+        }
+
+    }
+
+}
