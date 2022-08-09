6c2bad8db823131f2bac542b4ab58ddb30806341
muja-b <s11819212@stu.najah.edu>
2022-08-09 15:18:23 +0300

    fourth add added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Kata
         static void Main(string[] args)
         {
             KataStringCalcolator FactoredCalc = new KataStringCalcolator();
-            int ans = FactoredCalc.add("//;1;2");
+            int ans = FactoredCalc.add("3,44,1");
             Console.WriteLine(ans);
         }
 
@@ -33,26 +33,48 @@ namespace Kata
             ans -= Numbers.Where(x => x != ',').Count() * '0';
             return ans;
         }
+        private int tryAddbig(string Numbers)
+        {
+            List<string> Nums = Numbers.Split(',').ToList();
+            int sum = 0;
+            foreach (var item in Nums)
+            {
+                sum += Int32.Parse(item);
+            }
+
+            return sum;
+        }
         private int tryAddWithEndLine(string Numbers)
         {
-            int ans;
-            string endLine = "/n";
-            Numbers = Numbers.Replace("/n", "");
-            ans = Numbers.Where(x => x != ',').Aggregate((sum, i) => sum += i);
-            ans -= Numbers.Where(x => x != ',').Count() * '0';
-            return ans;
+            Numbers = Numbers.Replace("/n", ",");
+            List<string> Nums = Numbers.Split(',').ToList();
+            int sum = 0;
+            foreach (var item in Nums)
+            {
+                sum += Int32.Parse(item);
+            }
+
+            return sum;
         }
-         private int tryAdddelimiters(string Numbers)
+        private int tryAdddelimiters(string Numbers)
         {
-            int ans;
-            char endLine = Numbers[2];
-            Numbers = Numbers.Replace("/n", "");
-            Numbers = Numbers.Replace("//", "");
-            ans = Numbers.Where(x => x != endLine).Aggregate((sum, i) => sum += i);
-            ans -= Numbers.Where(x => x != endLine).Count() * '0';
-            return ans;
+            char endLine = ',';
+            if (Numbers.StartsWith("//"))
+            {
+                endLine = Numbers[2];
+                Numbers = Numbers.Remove(0,3);
+                Numbers = Numbers.Replace("/n", endLine + "");
+            }
+            List<string> Nums = Numbers.Split(endLine).ToList();
+            int sum = 0;
+            foreach (var item in Nums)
+            {
+                sum += Int32.Parse(item);
+            }
+
+            return sum;
         }
-        
+
 
     }
 
